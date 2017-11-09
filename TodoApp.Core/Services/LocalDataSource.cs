f08f8f8c33540d1c0bfe291372d3a222bd5e4d17
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 10:56:32 +0700

    Implement LocalDataSource

---
+++ TodoApp.Core/Services/LocalDataSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Core.Models;
+
+namespace TodoApp.Core.Services
+{
+    public class LocalDataSource : IDataSource
+    {
+        public IList<Item> Items { get; }
+
+        public LocalDataSource()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item);
+        }
+
+        public void RemoveItem(int itemId)
+        {
+            foreach (Item item in Items)
+            {
+                if (item.Id == itemId)
+                {
+                    Items.Remove(item);
+                    break;
+                }
+            }
+        }
+    }
+}

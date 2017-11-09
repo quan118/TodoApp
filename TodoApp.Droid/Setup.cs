26559da8e8d2133f898bfc8e60bb5e9fcc825cac
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 13:27:27 +0700

    Implement Setup.cs

---
+++ TodoApp.Droid/Setup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Platform;
+using TodoApp.Core;
+using Android.Content;
+
+namespace TodoApp.Droid
+{
+    public class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext) : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}

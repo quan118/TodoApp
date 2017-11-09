4d2532ca946d11f7f4bbc42702002e56795fa8de
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 13:34:11 +0700

    Create and Implement ItemListView Activity

---
+++ TodoApp.Droid/Views/ItemListView.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+using MvvmCross.Platform;
+using TodoApp.Core.Services;
+using TodoApp.Core.Models;
+using TodoApp.Core.ViewModels;
+
+namespace TodoApp.Droid.Views
+{
+    [Activity(Label = "ItemListView")]
+    public class ItemListView : MvxActivity
+    {
+        protected ItemListViewModel AssociatedViewModel
+        {
+            get { return ViewModel as ItemListViewModel; }
+        }
+
+        protected override void OnViewModelSet()
+        {
+            SetContentView(Resource.Layout.ItemListView);
+        }
+    }
+}

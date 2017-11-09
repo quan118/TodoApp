38b5add4ef0225bdf2e89493dca593db80f29f18
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 10:52:37 +0700

    Implement IDataSource

---
+++ TodoApp.Core/Services/IDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Core.Models;
+
+namespace TodoApp.Core.Services
+{
+    public interface IDataSource
+    {
+        IList<Item> Items { get; }
+        void AddItem(Item item);
+        void RemoveItem(Item item);
+        void RemoveItem(int itemId);
+    }
+}

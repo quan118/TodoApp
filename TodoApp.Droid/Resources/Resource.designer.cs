cf8f3b18724e4d9fe404403ef370a94962d05cff
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 13:24:16 +0700

    Implement SplashScreenActivity.cs, adding resources to drawable, implement SplashScreen.axml

--- TodoApp.Droid/Resources/Resource.designer.cs
+++ TodoApp.Droid/Resources/Resource.designer.cs
@@ -83,14 +83,51 @@ namespace TodoApp.Droid
 			}
 		}
 		
-		public partial class Id
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int MvvmCrossTagId = 2130837504;
+			public const int ic_action_content_create = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int MvxBindingTagUnique = 2130837505;
+			public const int ic_action_content_save = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int ic_menu_copy_holo_dark = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int ic_menu_cut_holo_dark = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int ic_menu_paste_holo_dark = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int ic_menu_search_holo_dark = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int ic_menu_trash = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int icon = 2130837511;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int MvvmCrossTagId = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int MvxBindingTagUnique = 2130968577;
 			
 			static Id()
 			{
@@ -102,11 +139,27 @@ namespace TodoApp.Droid
 			}
 		}
 		
-		public partial class String
+		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			public const int SplashScreen = 2130903040;
+			
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Layout()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int app_name = 2131034112;
 			
 			static String()
 			{

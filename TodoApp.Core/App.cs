f351ed19521cd43836cdbdf22ec51116d99f402d
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 13:14:24 +0700

    Implement App.cs

--- TodoApp.Core/App.cs
+++ TodoApp.Core/App.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using TodoApp.Core.ViewModels;
+using TodoApp.Core.Services;
+
 namespace TodoApp.Core
 {
-    public class App
+    public class App : MvxApplication
     {
         public App()
         {
+            Mvx.RegisterSingleton<IDataSource>(new LocalDataSource());
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<ItemListViewModel>());
         }
     }
 }

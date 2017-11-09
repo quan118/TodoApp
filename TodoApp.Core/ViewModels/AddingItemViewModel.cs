048489bfcb8c61da2d64d4dc88d183052ec61c5a
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 14:13:41 +0700

    Implement AddingItemViewModel.cs

---
+++ TodoApp.Core/ViewModels/AddingItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace TodoApp.Core.ViewModels
+{
+    public class AddingItemViewModel : MvxViewModel
+    {
+
+        private String _title;
+        public String Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+                RaisePropertyChanged(() => Title);
+            }
+        }
+
+        private String _description;
+        public String Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+                RaisePropertyChanged(() => Description);
+            }
+        }
+
+        public AddingItemViewModel()
+        {
+        }
+    }
+}

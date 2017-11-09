95ca1c3e4686263882ecc6fc344d7d4296cf512a
Quan Nguyen <quan.nguyen@niteco.se>
2017-11-09 10:28:58 +0700

    Create Item class

---
+++ TodoApp.Core/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TodoApp.Core.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public bool IsActive { get; set; }
+
+        public Item(int id, string title, string detail, bool isActive)
+        {
+            Id = id;
+            Title = title;
+            Detail = detail;
+            IsActive = isActive;
+        }
+    }
+}
